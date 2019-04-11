352e467570f23413a3e1c2504d69db21dbb57d1a
alexjhannan <alexjhannan@gmail.com>
2019-04-11 17:52:27 -0400

    random

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,36 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+const List = styled.ul`
+  list-style: none;
+  margin: 0
+`
+
+const ListItem = styled.li`
+  margin: 0;
+`
+
+const LINKS = [
+  { to: '/explosion-play', title: 'Explosion' },
+  { to: '/name-animation', title: 'Name v1' },
+  { to: '/name-animation-v2', title: 'Name v2' },
+]
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`react`, `developer`, `brooklyn`]} />
-    <h1>Hi</h1>
-    <p>Keep it up.</p>
-    <Link to="/animation-play/">Animation Play</Link>
+    <h1>Hey</h1>
+    <List>
+      {LINKS.map(({ to, title }) => (
+        <ListItem key={title}>
+          <Link to={to}>{title}</Link>
+        </ListItem>
+      ))}
+    </List>
   </Layout>
 )
 
