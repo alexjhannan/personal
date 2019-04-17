cba1c509450c35a659e7fc5d32ac093003134298
alexjhannan <alexjhannan@gmail.com>
2019-04-17 12:03:22 -0400

    add layout to guitar; refactor index

--- src/pages/guitar.js
+++ src/pages/guitar.js
@@ -1,2 +1,17 @@
+
+import React from "react"
+import { Link } from "gatsby"
+import styled from "styled-components"
+
+import Layout from "@components/Layout"
+import SEO from "@components/SEO"
 import Guitar from "@components/Guitar"
-export default Guitar
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Guitar Tool" keywords={[`react`, `guitar`]} />
+    <Guitar />
+  </Layout>
+)
+
+export default IndexPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,29 @@ import styled from "styled-components"
 import Layout from "@components/Layout"
 import SEO from "@components/SEO"
 
+const SECTIONS = [
+  {
+    header: 'Animations',
+    links: [
+      { to: '/explosion-play', title: 'Explosion' },
+      { to: '/web-dev-credits', title: 'Web Dev Credits' },
+    ],
+  },
+  {
+    header: 'Tools',
+    links: [
+      { to: '/guitar', title: 'Guitar' },
+    ],
+  },
+  {
+    header: 'Other Profiles',
+    links: [
+      { to: 'https://www.github.com/alexjhannan', title: 'Github' },
+      { to: 'https://www.linkedin.com/in/alexjhannan', title: 'LinkedIn' },
+    ],
+  },
+]
+
 const ListHeader = styled.h3`
   margin: 1.45rem 0 0;
 `
@@ -18,46 +41,11 @@ const ListItem = styled.li`
   margin: 0;
 `
 
-const ANIMATION_LINKS = [
-  { to: '/explosion-play', title: 'Explosion' },
-  { to: '/web-dev-credits', title: 'Web Dev Credits' },
-]
-
-const TOOL_LINKS = [
-  { to: '/guitar', title: 'Guitar' },
-]
-
-const PROFESSIONAL_LINKS = [
-  { to: 'https://www.github.com/alexjhannan', title: 'Github' },
-  { to: 'https://www.linkedin.com/in/alexjhannan', title: 'LinkedIn' },
-]
-
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`react`, `developer`, `brooklyn`]} />
-    <ListHeader>Animations:</ListHeader>
-    <List>
-      {ANIMATION_LINKS.map(({ to, title }) => (
-        <ListItem key={title}>
-          <Link to={to}>{title}</Link>
-        </ListItem>
-      ))}
-    </List>
-    <ListHeader>Tools:</ListHeader>
-    <List>
-      {TOOL_LINKS.map(({ to, title }) => (
-        <ListItem key={title}>
-          {to.match(/^http/) ? (
-            <Link to={to}>{title}</Link>
-          ) : (
-            <a href={to}>{title}</a>
-          )}
-        </ListItem>
-      ))}
-    </List>
-    <ListHeader>Professional Stuff:</ListHeader>
-    <List>
-      {PROFESSIONAL_LINKS.map(({ to, title }) => (
+const LinkList = ({ header, links }) => (
+  <>
+    <ListHeader>{header}:</ListHeader>
+      <List>
+      {links.map(({ to, title }) => (
         <ListItem key={title}>
           {/^http/.test(to) ? (
             <a href={to} target="_blank" rel="noopener noreferrer">{title}</a>
@@ -67,6 +55,15 @@ const IndexPage = () => (
         </ListItem>
       ))}
     </List>
+  </>
+)
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" keywords={[`react`, `developer`, `brooklyn`]} />
+    { SECTIONS.map((sectionProps) => (
+      <LinkList {...sectionProps} />
+    ))}
   </Layout>
 )
 
