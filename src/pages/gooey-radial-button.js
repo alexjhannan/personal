54d630c5effb47abc1e2e4b0359b041ec230314f
alexjhannan <alexjhannan@gmail.com>
2019-04-26 16:26:14 -0400

    update gooey radial button text

--- src/pages/gooey-radial-button.js
+++ src/pages/gooey-radial-button.js
@@ -4,7 +4,7 @@ import Layout from '~components/layout'
 
 const GooeyRadialButton = () => (
   <Layout>
-    <ScaleButton scaleName="Major" scaleActionCurry={() => {}} />
+    <ScaleButton scaleName="click" scaleActionCurry={() => {}} />
   </Layout>
 )
 
